5373d4ef7e328e244a907f8862d96cacf958feda
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-09-19 17:22:08 +0300

    Files updated

--- dadata_api.py
+++ dadata_api.py
@@ -2,3 +2,12 @@ import json
 import requests
 import os
 
+
+class DadataAddress:
+    """Класс AddressDadata с методами для отправки REST API-запросов к сервисному модулю "API: стандартизация адресов"
+    на сайте https://dadata.ru/api/clean/address/. Класс использует стандартную библиотеку Python - requests."""
+
+    def __init__(self):
+        self.base_url = 'https://cleaner.dadata.ru/api/v1/clean/address'
+
+    def get
--- settings/settings.py
+++ settings/settings.py
@@ -2,5 +2,5 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-valid_key = os.getenv('valid_key')
-secret_key = os.getenv('secret_key')
+token = os.getenv('token')
+secret = os.getenv('secret')
