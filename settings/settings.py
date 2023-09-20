f488d3eddda009151b6b287dc1fedf1a68ca2256
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-09-20 18:08:40 +0300

    Files updated

--- settings/settings.py
+++ settings/settings.py
@@ -4,3 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 token = os.getenv('token')
 secret = os.getenv('secret')
+invalid_token = os.getenv('invalid_token')
+invalid_secret = os.getenv('invalid_secret')
--- tests/test_dadata_api.py
+++ tests/test_dadata_api.py
@@ -1,25 +1,44 @@
 """Коллекция авто-тестов для проверки отправки запросов на REST API сервис https://dadata.ru/. Для формирования тестовых
 запросов импортирована библиотека от разработчика сервиса - Dadata."""
-
+import requests
 from dadata import Dadata
 import pytest
-import requests
+from requests import *
 import json
+import http_api_exception
 from settings.settings import *
 
 Dd = Dadata(token, secret)
 
 
-def test_get_address_info_valid_data():
-    """Позитивный тест с валидными данными на проверку post-запроса к услуге "Разбор адреса из строки («стандартизация»)
-    api-сервиса https://dadata.ru/. Тестируется корректная обработка введенного пользователем адреса, намеренно
-    содержащего ошибки и сокращения """
+# def test_get_address_info_valid_data():
+#     """Позитивный тест с валидными данными на проверку post-запроса к услуге "Разбор адреса из строки («стандартизация»)
+#     api-сервиса https://dadata.ru/. Тестируется корректная обработка и разбор введенного пользователем адреса,
+#     намеренно содержащего ошибки и сокращения."""
+#
+#     response = Dd.clean('address', source='мск, металургов, дом 58,')
+#     assert response['region_with_type'] == 'г Москва'
+#     assert response['street'] == 'Металлургов'
+#     assert response['house'] == '58'
+#     assert response['house_type'] == 'д'
+#     assert response['postal_code'] == '111399'
 
-    response = Dd.clean('address', source='мск, металургов, дом 58,')
-    assert response['region_with_type'] == 'г Москва'
-    assert response['street'] == 'Металлургов'
-    assert response['house'] == '58'
-    assert response['house_type'] == 'д'
-    assert response['postal_code'] == '111399'
+def test_get_address_info_invalid_data():
+    """Негативный тест с не валидными данными на проверку post-запроса к услуге "Разбор адреса из строки
+    («стандартизация») api-сервиса https://dadata.ru/. Тестируется отправка и обработка post-запроса с некорретными
+    ключами token и secret. """
+
+    Dd = Dadata(invalid_token, invalid_secret)
+
+    try:
+        response = Dd.clean('address', source='мск, перовская, дом 13, корпус 1')
+    except http_api_exception.HTTPStatus:
+        print('Ошибка! Проверьте корректность ключей пользователя для отправки запроса!')
+
+
+
+
+
+
 
 
