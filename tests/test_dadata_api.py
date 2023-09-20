62fa87cd83bad552fd4d1e6fcec72bc22ef260a1
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-09-20 16:54:40 +0300

    Files updated

--- tests/test_dadata_api.py
+++ tests/test_dadata_api.py
@@ -12,9 +12,14 @@ Dd = Dadata(token, secret)
 
 def test_get_address_info_valid_data():
     """Позитивный тест с валидными данными на проверку post-запроса к услуге "Разбор адреса из строки («стандартизация»)
-    api-сервиса https://dadata.ru/. """
+    api-сервиса https://dadata.ru/. Тестируется корректная обработка введенного пользователем адреса, намеренно
+    содержащего ошибки и сокращения """
 
-    response = Dd.clean('address', source='МО, г. Видное, ул. Проспект Ленинского Комсомола, д. 2, корпус 4, кв. 83')
-    print(f" Адрес:{response['result']}\n Широта: {response['geo_lat']}', Долгота: {response['geo_lon']}'")
+    response = Dd.clean('address', source='мск, металургов, дом 58,')
+    assert response['region_with_type'] == 'г Москва'
+    assert response['street'] == 'Металлургов'
+    assert response['house'] == '58'
+    assert response['house_type'] == 'д'
+    assert response['postal_code'] == '111399'
 
 
