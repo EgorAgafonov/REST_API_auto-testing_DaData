0f086d17058d5a05c04f77d00fe1f915f3ce0a23
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-09-22 12:50:58 +0300

    Files updated

--- settings/settings.py
+++ settings/settings.py
@@ -11,15 +11,17 @@ user_balance = float(os.getenv('user_balance'))
 
 
 def duration_time_of_test(func):
+    """Функция-декоратор для определения длительности авто-теста с момента запуска\до момента его окончания."""
+
     def wrapper(*args, **kwargs):
         start_time = time.time()
-        print(f"Начало выполнения теста: {start_time}")
+        print(f"\nНачало выполнения теста: {round(start_time,3)}")
 
         func(*args, **kwargs)
 
         end_time = time.time()
-        print(f"Окончание выполнения теста: {end_time}")
+        print(f"\nОкончание выполнения теста: {round(end_time,3)}")
         result = time.time() - start_time
-        print(f"Общая продолжительность выполнения теста: {result}")
+        print(f"\nОбщая продолжительность выполнения теста: {round(result,3)})")
 
     return wrapper
