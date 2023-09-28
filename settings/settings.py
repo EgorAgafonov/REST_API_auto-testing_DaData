d5f702faa80a3a8c3b94c39ae1ed2a889c9ccfe3
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-09-28 10:31:43 +0300

    Files updated

--- settings/settings.py
+++ settings/settings.py
@@ -1,33 +1,36 @@
-import os
-import time
-from dotenv import load_dotenv
+# import os
+# import time
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+# token = os.getenv('token')
+# secret = os.getenv('secret')
+# invalid_token = os.getenv('invalid_token')
+# invalid_secret = os.getenv('invalid_secret')
+# user_balance = float(os.getenv('user_balance'))
+#
+#
+# def duration_time_of_test(func):
+#     """Функция-декоратор для определения длительности авто-теста с момента запуска\до момента его окончания."""
+#
+#     def wrapper(*args, **kwargs):
+#         start_time = time.time()
+#         print(f"\n\nНачало выполнения теста: {time.asctime()}")
+#
+#         func(*args, **kwargs)
+#
+#         end_time = time.time()
+#         print(f"Окончание выполнения теста: {time.asctime()}")
+#         result = end_time - start_time
+#         print(f"Общая продолжительность выполнения теста: {round(result, 3)} сек.")
+#
+#     return wrapper
 
-load_dotenv()
-token = os.getenv('token')
-secret = os.getenv('secret')
-invalid_token = os.getenv('invalid_token')
-invalid_secret = os.getenv('invalid_secret')
-user_balance = float(os.getenv('user_balance'))
+def summary(*nums):
+    a = []
+    for i in nums:
+        num = i * 2
+        a.append(num)
+    return a
 
-
-def duration_time_of_test(func):
-    """Функция-декоратор для определения длительности авто-теста с момента запуска\до момента его окончания."""
-
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        print(f"\n\nНачало выполнения теста: {time.asctime()}")
-
-        func(*args, **kwargs)
-
-        end_time = time.time()
-        print(f"Окончание выполнения теста: {time.asctime()}")
-        result = end_time - start_time
-        print(f"Общая продолжительность выполнения теста: {round(result,3)} сек.")
-
-    return wrapper
-
-
-x = {"a": 1, "b": 2}
-y = {"b": 3, "c": 4}
-z = {**x, **y}
-print(z)
+print(summary(10,12,100,3,56,9,8,72))
