31db244f8e912506b62fcbee534d05bad82b21a6
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-08-21 16:19:36 +0300

    New projects files added

---
+++ massives.py
@@ -0,0 +1,8 @@
+# 'the largest world', "world's famous toy"
+
+word = 'world'
+list_of_words = "world's famous toy"
+if 'world' in list_of_words:
+    print(f"Слово '{word}' содержится в списке элементов.")
+else:
+    print(f"Слова '{word}' в списке элементов нет.")
