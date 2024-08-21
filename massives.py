697613e44fe1b7e2e9d8f1f97dc2c7b021d94b1d
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-08-21 16:49:19 +0300

    New projects files added

--- massives.py
+++ massives.py
@@ -1,8 +1,14 @@
 # 'the largest world', "world's famous toy"
 
-word = 'world'
-list_of_words = "world's famous toy"
-if 'world' in list_of_words:
-    print(f"Слово '{word}' содержится в списке элементов.")
-else:
-    print(f"Слова '{word}' в списке элементов нет.")
+# word = 'world'
+# list_of_words = "the world isn't enough"
+# result = list_of_words.index(word)
+# print(result)
+# if 'world' in list_of_words:
+#     print(f"Слово '{word}' содержится в списке элементов.")
+# else:
+#     print(f"Слова '{word}' в списке элементов нет.")
+
+keys = ['name', 'surname', 'age']
+values = ['John', 'Smith', 26]
+print(dict(zip(keys, values)))
