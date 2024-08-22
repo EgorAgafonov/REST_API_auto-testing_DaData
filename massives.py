ad415a71cd8df6e441902cbc5194032f76f20e34
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-08-22 15:21:23 +0300

    New projects files added

--- massives.py
+++ massives.py
@@ -1,3 +1,5 @@
+from operator import itemgetter, attrgetter
+
 # 'the largest world', "world's famous toy"
 
 # word = 'world'
@@ -13,12 +15,28 @@
 # values = ['John', 'Smith', 26]
 # print(dict(zip(keys, values)))
 
-dictionary = {}
-print(id(dictionary))
-dictionary['name'] = 'Иван'
-second_dict = dictionary.copy()
-second_dict.update(surname='Павлов', age=34)
-print(dictionary)
-print(second_dict)
-del (second_dict['age'])
-print(second_dict)
+# dictionary = {}
+# print(id(dictionary))
+# dictionary['name'] = 'Иван'
+# second_dict = dictionary.copy()
+# second_dict.update(surname='Павлов', age=34)
+# print(dictionary)
+# print(second_dict)
+# del (second_dict['surname'])
+# print(second_dict)
+
+
+class Capital:
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
+
+    def __repr__(self):
+        return repr((self.name, self.population))
+
+
+population = [('Pekin', 2234441233), ('Moscow', 123331233), ('London', 300123), ('Amsterdam', 3312333)]
+res = sorted(population, key=lambda capital: capital[1], reverse=True)
+
+# res = sorted(population, key=attrgetter('value'))
+print(res)
