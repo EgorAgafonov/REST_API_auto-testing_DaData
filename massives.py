ea61d534cf36fe64b1a340038d8251c755158d95
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-08-22 15:56:07 +0300

    New projects files added

--- massives.py
+++ massives.py
@@ -1,20 +1,25 @@
 from operator import itemgetter, attrgetter
 
+# 1. Поиск совпадения в строке по шаблону (word):
 
-# 'the largest world', "world's famous toy"
+word = 'world'
+list_of_words = "the world isn't enough"
+result = list_of_words.index(word)
+print(result)
 
-# word = 'world'
-# list_of_words = "the world isn't enough"
-# result = list_of_words.index(word)
-# print(result)
-# if 'world' in list_of_words:
-#     print(f"Слово '{word}' содержится в списке элементов.")
-# else:
-#     print(f"Слова '{word}' в списке элементов нет.")
+if 'world' in list_of_words:
+    print(f"Слово '{word}' содержится в списке элементов.")
+else:
+    print(f"Слова '{word}' в списке элементов нет.")
 
-# keys = ['name', 'surname', 'age']
-# values = ['John', 'Smith', 26]
-# print(dict(zip(keys, values)))
+# 2. Объединение(распаковка - zip) значений разных списков в один словарь:
+
+keys = ['name', 'surname', 'age']
+values = ['John', 'Smith', 26]
+
+print(dict(zip(keys, values)))
+
+# 3. Добавление и удаление ключей словаря:
 
 # dictionary = {}
 # print(id(dictionary))
@@ -26,25 +31,26 @@ from operator import itemgetter, attrgetter
 # del (second_dict['surname'])
 # print(second_dict)
 
+# 4. Сортировка элементов списков, являющихся объектами созданного нами класса:
 
-class Capital:
-    def __init__(self, name, popul):
-        self.name = name
-        self.popul = popul
-
-    def __repr__(self):
-        return repr((self.name, self.popul))
-
-
-list_of_cap = [
-    Capital('Pekin', 2234441233),
-    Capital('Moscow', 123331233),
-    Capital('London', 300123),
-    Capital('Amsterdam', 3312333)
-]
-
-res = sorted(list_of_cap, key=lambda capital: capital.popul, reverse=True)
+# class Capital:
+#     def __init__(self, name, popul):
+#         self.name = name
+#         self.popul = popul
+#
+#     def __repr__(self):
+#         return repr((self.name, self.popul))
+#
+#
+# list_of_cap = [
+#     Capital('Pekin', 2234441233),
+#     Capital('Moscow', 123331233),
+#     Capital('London', 300123),
+#     Capital('Amsterdam', 3312333)
+# ]
+#
+# res = sorted(list_of_cap, key=lambda capital: capital.popul, reverse=True)
 # res = sorted(list_of_cap, key=attrgetter('popul'), reverse=True)
 # res = sorted(list_of_cap, key=attrgetter('name'))
-
-print(res)
+#
+# print(res)
